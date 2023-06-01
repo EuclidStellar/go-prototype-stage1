40020e96728e3d8b36adada51cc03a6928f6a388
Euclid Stellar <euclidstellar@gmail.com>
2023-06-01 14:49:26 +0530

    user input added

---
+++ 03userinput/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	welcome := "welcome to user input "
+
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("enter the number : ")
+
+	//comma ok syntax || error ok syntax
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("thanks for the inputs, ", input)
+	fmt.Printf("datatype of entered number : %T \n ", input)
+
+}

c5186c4aacf535beca984824f9099c293d9343a5
Euclid Stellar <euclidstellar@gmail.com>
2023-06-01 19:38:34 +0530

    time added

---
+++ 05mytime/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("welcome to time study of golang ")
+	presenttime := time.Now()
+	fmt.Println(presenttime)
+	fmt.Println(presenttime.Format("01-04-2006  15:04:05 Monday"))
+
+	createwddate := time.Date(2005, time.April, 25, 4, 4, 5, 8, time.UTC)
+	fmt.Println(createwddate)
+	fmt.Println(createwddate.Format("01-02-2006 Monday"))
+
+}

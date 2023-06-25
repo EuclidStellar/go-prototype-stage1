e2c530b54848ca9de32000086efd56eef0413df3
Gaurav Singh <100860877+EuclidStellar@users.noreply.github.com>
2023-06-25 17:15:14 +0530

    Update main.go

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println(presenttime)
 	fmt.Println(presenttime.Format("01-04-2006  15:04:05 Monday"))
 
+	
 	createwddate := time.Date(2005, time.April, 25, 4, 4, 5, 8, time.UTC)
 	fmt.Println(createwddate)
 	fmt.Println(createwddate.Format("01-02-2006 Monday"))
